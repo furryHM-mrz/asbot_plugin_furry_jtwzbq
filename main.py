268ca4ead38baa73888afbda40cda1e1771ff3a8
furry豪毛 <sb159815981598@163.com>
2025-07-25 16:44:29 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -11,56 +11,56 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
     # 监听文本消息，当消息内容为awa时触发
-    @filter.text("awa")  # 筛选文本内容为awa的消息
+    @filter.command("awa")  # 筛选文本内容为awa的消息
     async def reply_awa(self, event: AstrMessageEvent):
         """监听到awa时回复awa"""
         # 直接回复固定文本"awa"
         yield event.plain_result("awa")
 
     # 监听文本消息，当消息内容为qwq时触发
-    @filter.text("qwq")  # 筛选文本内容为qwq的消息
+    @filter.command("qwq")  # 筛选文本内容为qwq的消息
     async def reply_qwq(self, event: AstrMessageEvent):
         """监听到qwq时回复qwq"""
         # 直接回复固定文本"qwq"
         yield event.plain_result("qwq")
 
     # 监听文本消息，当消息内容为uwu时触发
-    @filter.text("uwu")
+    @filter.command("uwu")
     async def reply_uwu(self, event: AstrMessageEvent):
         """监听到uwu时回复uwu"""
         # 直接回复固定文本"uwu"
         yield event.plain_result("uwu")
 
     # 监听文本消息以此类推，懒得写注释。
-    @filter.text("QAQ")
+    @filter.command("QAQ")
     async def reply_QAQ(self, event: AstrMessageEvent):
         yield event.plain_result("QAQ")
 
-    @filter.text("owo")
+    @filter.command("owo")
     async def reply_owo(self, event: AstrMessageEvent):
         yield event.plain_result("owo")
 
-    @filter.text("sws")
+    @filter.command("sws")
     async def reply_sws(self, event: AstrMessageEvent):
         yield event.plain_result("sws")
 
-    @filter.text("xwx")
+    @filter.command("xwx")
     async def reply_xwx(self, event: AstrMessageEvent):
         yield event.plain_result("xwx")
 
-    @filter.text("owo")
+    @filter.command("owo")
     async def reply_owo(self, event: AstrMessageEvent):
         yield event.plain_result("owo")
 
-    @filter.text("zwz")
+    @filter.command("zwz")
     async def reply_zwz(self, event: AstrMessageEvent):
         yield event.plain_result("zwz")
 
-    @filter.text("hwh")
+    @filter.command("hwh")
     async def reply_hwh(self, event: AstrMessageEvent):
         yield event.plain_result("hwh")
 
-    @filter.text("xvx")
+    @filter.command("xvx")
     async def reply_xvx(self, event: AstrMessageEvent):
         yield event.plain_result("xvx")
 
